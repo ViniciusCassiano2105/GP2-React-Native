f8f6fa7a402085079bc9155f048d09685fe215e6
arcarreiro <arcarreiro@gmail.com>
2024-11-14 16:59:50 -0300

    Commit inicial

---
+++ space-clicker/src/components/Banner/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export const Placar = () => {
+    return(
+        <View>
+            
+        </View>
+    )
+}
