76a18a3ebd9dd66efb3e3bb3de2038e10c66a6c8
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-11-18 21:54:41 -0300

    Trilha sonora - Placar

--- space-clicker/src/pages/Placar/index.tsx
+++ space-clicker/src/pages/Placar/index.tsx
@@ -1,15 +1,45 @@
-import React from "react";
-import { View, SafeAreaView, Text,} from "react-native";
-import { Video } from "expo-av";
+import React, { useEffect, useState } from "react";
+import { View, SafeAreaView, Text } from "react-native";
+import { Video, Audio } from "expo-av";
 import { styles } from "./styles";
 
 export const Placar = () => {
+  const [sound, setSound] = useState<Audio.Sound | undefined>();
+
+  // Função para carregar e tocar a música
+  async function playMusic() {
+    try {
+      const { sound } = await Audio.Sound.createAsync(
+        require("./MarioKartDoubleDashMusic.mp3"),
+        {
+          shouldPlay: true, // Inicia automaticamente
+          isLooping: true, // Reproduz em loop
+        }
+      );
+      setSound(sound); // Define o objeto `sound` no estado
+    } catch (error) {
+      console.error("Erro ao carregar o áudio:", error);
+    }
+  }
+
+  // Tocar música quando o componente for montado
+  useEffect(() => {
+    playMusic();
+
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
+      {/* Vídeo como fundo */}
       <Video
-        style={styles.video} 
+        style={styles.video}
         source={require("./background.mp4")} 
-        resizeMode="cover" 
+        resizeMode="cover"
         isLooping
         shouldPlay
       />
@@ -18,8 +48,6 @@ export const Placar = () => {
           <Text style={styles.player}>PLAYER</Text>
           <Text style={styles.score}>SCORE</Text>
         </View>
-
-       
       </SafeAreaView>
     </View>
   );
