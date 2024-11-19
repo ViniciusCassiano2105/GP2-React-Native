900909abcb7491f6549190d7778fc85e5ac26d2c
Gabriel Toledo <gabrieltoledo4629@gmail.com>
2024-11-19 18:10:44 -0300

    Adicionando components de colocação

---
+++ space-clicker/src/components/Colocacao/Player/style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: "#333",
+    borderRadius: 8,
+    marginVertical: 4,
+  },
+  text: {
+    fontSize: 16,
+    color: "#fff",
+  },
+});
---
+++ space-clicker/src/components/Colocacao/Score/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: "#555",
+    borderRadius: 8,
+    marginVertical: 4,
+  },
+  text: {
+    fontSize: 16,
+    color: "#FFD700", 
+    fontWeight: "bold",
+  },
+});
