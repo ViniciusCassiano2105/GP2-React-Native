89da413915c882fda8261c5c20b374edc26a0e30
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-11-18 21:55:31 -0300

    Ajustes

--- space-clicker/src/pages/Placar/index.tsx
+++ space-clicker/src/pages/Placar/index.tsx
@@ -1,28 +1,26 @@
+import { Audio, Video } from "expo-av";
 import React, { useEffect, useState } from "react";
-import { View, SafeAreaView, Text } from "react-native";
-import { Video, Audio } from "expo-av";
+import { SafeAreaView, Text, View } from "react-native";
 import { styles } from "./styles";
 
 export const Placar = () => {
   const [sound, setSound] = useState<Audio.Sound | undefined>();
 
-  // Função para carregar e tocar a música
   async function playMusic() {
     try {
       const { sound } = await Audio.Sound.createAsync(
         require("./MarioKartDoubleDashMusic.mp3"),
         {
-          shouldPlay: true, // Inicia automaticamente
-          isLooping: true, // Reproduz em loop
+          shouldPlay: true,
+          isLooping: true,
         }
       );
-      setSound(sound); // Define o objeto `sound` no estado
+      setSound(sound);
     } catch (error) {
       console.error("Erro ao carregar o áudio:", error);
     }
   }
 
-  // Tocar música quando o componente for montado
   useEffect(() => {
     playMusic();
 
@@ -35,10 +33,9 @@ export const Placar = () => {
 
   return (
     <View style={styles.container}>
-      {/* Vídeo como fundo */}
       <Video
         style={styles.video}
-        source={require("./background.mp4")} 
+        source={require("./background.mp4")}
         resizeMode="cover"
         isLooping
         shouldPlay
