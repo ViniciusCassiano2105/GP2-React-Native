0dcab351b84bdc69ed92fe1c72aee9eb653a95ed
weliton schitini <schitini1987@gmail.com>
2024-11-24 16:32:37 -0300

    Modificação do tamanho do header e posicionamento dos seus componentes

--- space-clicker/src/components/Banner/headerStyles.ts
+++ space-clicker/src/components/Banner/headerStyles.ts
@@ -6,29 +6,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     backgroundColor: '#000',
-    height: 90,
+    height: 150,
     paddingVertical: 5,
     paddingHorizontal: 25,
+    
 
   },
   leftElement: {
     flex: 1,
     alignItems: 'flex-start',
+    top:20
   },
   title: {
     flex: 1,
     alignItems: 'center',
     justifyContent: "center",
+    top:20
+
   },
   titleText: {
     fontSize: 14,
     fontWeight: 'bold',
     flexShrink: 1,
+    top:20
+
 
   },
   rightElement: {
     flex: 1,
     alignItems: 'flex-end',
+    top:20
+
   },
 });
 
--- space-clicker/src/components/Timer/styles.ts
+++ space-clicker/src/components/Timer/styles.ts
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
+    top:30,
     flex: 1, 
     justifyContent: "center", 
     alignItems: "center", 
