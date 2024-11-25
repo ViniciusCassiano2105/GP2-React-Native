59b6bc94c3c72965f94e60ec252df8889033c898
arcarreiro <arcarreiro@gmail.com>
2024-11-25 17:18:59 -0300

    Revert "adição do timer de 3 segundos antes de começar o jogo"
    This reverts commit a03e66f9942a2c8ae144a3373ede1a7e7e48ebf6.

--- space-clicker/src/components/Game/styles.ts
+++ space-clicker/src/components/Game/styles.ts
@@ -1,74 +1,45 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-<<<<<<< Updated upstream
-  container: {
-    flex: 1,
-    margin: 0,
-    padding: 0,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  backgroundImage: {
-    ...StyleSheet.absoluteFillObject, 
-    zIndex: -2, 
-  },
-  video: {
-    ...StyleSheet.absoluteFillObject,
-    zIndex: -1,
-  },
-  spaceShip: {
-    resizeMode: "center",
-  },
-  botao: {
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
-    zIndex: 0,
-  },
-  botaoStart: {
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 1,
-  },
-  timer:{
-    color:"red",
-    fontWeight:"bold",
-    fontSize:150,
-    
-  }
-});
-=======
-    video: {
-        ...StyleSheet.absoluteFillObject,
-        zIndex: -1,
-    },
 
     container: {
         flex: 1,
         margin: 0,
         padding: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
     },
-
+    backgroundImage: {
+        ...StyleSheet.absoluteFillObject,
+        zIndex: -2,
+    },
+    video: {
+        ...StyleSheet.absoluteFillObject,
+        zIndex: -1,
+    },
     spaceShip: {
-        resizeMode: "contain"
+        resizeMode: "contain",
     },
-
     botao: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute'
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        zIndex: 0,
+    },
+    botaoStart: {
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 1,
     },
 
+    timer: {
+        color: "red",
+        fontWeight: "bold",
+        fontSize: 150,
+
+    },
     background: {
         flex: 1,
     },
+});
 
-    botaoStart: {
-        justifyContent: 'center',
-        alignItems:'center',
-    }
-})
->>>>>>> Stashed changes
